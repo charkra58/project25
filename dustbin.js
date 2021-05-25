71716d7d7eb66c2f573ac3dc2e509982a80b85f9
charkra58 <81859035+charkra58@users.noreply.github.com>
2021-05-25 10:42:31 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,11 @@
+class Dustbin{
+    constructor(){
+      
+    }
+    log1 = new this.log1(400,550,20,100);
+    log2 = new this.log2(550,550,20,100);
+    log3 = new this.log3(200,420,20,100);
+
+
+
+}
---
+++ sketch.js
@@ -0,0 +1,42 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	this.Bodies
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background("black");
+  
+
+  log1.display();
+  log2.display();
+  log3.display();
+
+  
+  drawSprites();
+ 
+}
+
+
+
